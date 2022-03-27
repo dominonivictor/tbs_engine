dd4bafc4740d87bb1c827d2e15c55d7bbb52f17a
victor <dominoni.victor@gmail.com>
2022-03-27 09:31:13 -0300

    feat: add basic attacks with tests

--- main.go
+++ main.go
@@ -1,18 +1,107 @@
-package example
+package main
 
-import (
-	"testing"
+import "fmt"
+
+// ENUMS
+type DMG_TYPES int8
+
+const (
+	DIRECT_DMG_TYPE DMG_TYPES = iota
+	TRUE_DMG_TYPE
+	NONE_DMG_TYPE
 )
 
-func TestSimpleAtk(t *testing.T) {
-	stat := Stats{hp: 10, hpMax: 10, atk: 1, def: 1}
-	a1 := Actor{name: "a1", stats: stat}
-	a2 := Actor{name: "a2", stats: stat}
-	s := Skill{name: "s1", dmgValue: 5, dmgType: DIRECT}
+type SKILL_TYPES int8
 
-	act(s, &a1, &a2)
+const (
+	ATK_SKILL_TYPE SKILL_TYPES = iota
+	BUFF_SKILL_TYPE
+)
 
-	if a2.stats.hp != 5 {
-		t.Errorf("Actor hp hit by skill should be 5, was %v", a2)
+// STRUCTS
+
+type Skill struct {
+	name        string
+	value       int8
+	dmgType     DMG_TYPES
+	isFlatValue bool
+	skillType   SKILL_TYPES
+}
+
+type Stats struct {
+	hp    int8
+	maxHP int8
+	atk   int8
+	def   int8
+}
+
+type Actor struct {
+	name  string
+	stats Stats
+}
+
+// FUNCTIONS
+func validateDmg(dmg int8) int8 {
+	if dmg < 0 {
+		return int8(0)
 	}
+	return dmg
+}
+
+func dealDmg(target *Actor, dmg int8) {
+	if target.stats.hp-dmg < 0 {
+		target.stats.hp = 0
+	}
+	target.stats.hp -= dmg
+}
+
+func chooseAtkValue(s Skill, owner *Actor) int8 {
+	if s.isFlatValue {
+		return s.value
+	}
+	return owner.stats.atk + s.value
+
+}
+
+func actAtkDirect(s Skill, owner, target *Actor) {
+	atk := chooseAtkValue(s, owner)
+	def := target.stats.def
+	dmg := atk - def
+	dmg = validateDmg(dmg)
+	dealDmg(target, dmg)
+}
+
+func actAtkTrue(s Skill, owner, target *Actor) {
+	dmg := chooseAtkValue(s, owner)
+	dealDmg(target, dmg)
+}
+
+func actAtk(s Skill, owner, target *Actor) {
+	var fun func(Skill, *Actor, *Actor)
+	switch s.dmgType {
+	case DIRECT_DMG_TYPE:
+		fun = actAtkDirect
+	case TRUE_DMG_TYPE:
+		fun = actAtkTrue
+	}
+
+	fun(s, owner, target)
+}
+
+func chooseSkillFunc(s Skill) func(Skill, *Actor, *Actor) {
+	var fun func(Skill, *Actor, *Actor)
+	switch s.skillType {
+	case ATK_SKILL_TYPE:
+		fun = actAtk
+	}
+	return fun
+}
+
+func act(s Skill, owner, target *Actor) {
+	fun := chooseSkillFunc(s)
+	fun(s, owner, target)
+}
+
+func main() {
+	fmt.Println("Initializing...")
 }
--- main_test.go
+++ main_test.go
@@ -1,18 +1,57 @@
-package example
+package main
 
 import (
+	//tbs "tbs_engine/main"
 	"testing"
 )
 
-func TestSimpleAtk(t *testing.T) {
-	stat := Stats{hp: 10, hpMax: 10, atk: 1, def: 1}
-	a1 := Actor{name: "a1", stats: stat}
-	a2 := Actor{name: "a2", stats: stat}
-	s := Skill{name: "s1", dmgValue: 5, dmgType: DIRECT}
+func TestAtkSkills(t *testing.T) {
+	stat := Stats{hp: 10, maxHP: 10, atk: 1, def: 2}
+	owner := Actor{name: "a1", stats: stat}
+	target := Actor{name: "a2", stats: stat}
+	t.Run("Direct Atk without Flat Dmg", func(t *testing.T) {
+		s := Skill{name: "s1", value: 4, dmgType: DIRECT_DMG_TYPE}
 
-	act(s, &a1, &a2)
+		act(s, &owner, &target)
 
-	if a2.stats.hp != 5 {
-		t.Errorf("Actor hp hit by skill should be 5, was %v", a2)
-	}
+		want := target.stats.maxHP - 3
+		if target.stats.hp != want {
+			t.Errorf("Actor hp hit by skill should be %v, was %+v", want, target)
+		}
+	})
+	t.Run("Direct Atk with Flat Dmg", func(t *testing.T) {
+		target.stats.hp = target.stats.maxHP
+		s := Skill{name: "s1", value: 4, dmgType: DIRECT_DMG_TYPE, isFlatValue: true}
+
+		act(s, &owner, &target)
+
+		want := target.stats.maxHP - 2
+		if target.stats.hp != want {
+			t.Errorf("Actor hp hit by skill should be %v, was %+v", want, target)
+		}
+	})
+	t.Run("True Atk without Flat Dmg", func(t *testing.T) {
+		target.stats.hp = target.stats.maxHP
+		s := Skill{name: "s1", value: 5, dmgType: TRUE_DMG_TYPE}
+
+		act(s, &owner, &target)
+
+		want := target.stats.maxHP - 6
+		if target.stats.hp != want {
+			t.Errorf("Actor hp hit by skill should be %v, was %+v", want, target)
+		}
+
+	})
+	t.Run("True Atk with Flat Dmg", func(t *testing.T) {
+		target.stats.hp = target.stats.maxHP
+		s := Skill{name: "s1", value: 5, dmgType: TRUE_DMG_TYPE, isFlatValue: true}
+
+		act(s, &owner, &target)
+
+		want := target.stats.maxHP - 5
+		if target.stats.hp != want {
+			t.Errorf("Actor hp hit by skill should be %v, was %+v", want, target)
+		}
+
+	})
 }
